f20fab9009fe4bc8b66f09e301859668c7d61b48
masolivs <marianasoli621@gmail.com>
2024-04-24 13:48:40 -0300

    Mais alguns testes.

--- romanos.cpp
+++ romanos.cpp
@@ -1,5 +1,28 @@
 #include "romanos.hpp"
 int converterRomanoParaArabico(const std::string& numeroRomano){
-  return -1; 
+  int arabico = 0;
+  int valorAnterior = 0;
+
+  for (int i = numeroRomano.size() - 1; i >= 0; --i) {
+    char algarismo = numeroRomano[i];
+        int valor = 0;
+        switch (algarismo) {
+            case 'I': valor = 1; break;
+            case 'V': valor = 5; break;
+            case 'X': valor = 10; break;
+            case 'L': valor = 50; break;
+            case 'C': valor = 100; break;
+            case 'D': valor = 500; break;
+            case 'M': valor = 1000; break;
+            default: return -1;
+  }
+  if (valor < valorAnterior) {
+    arabico -= valor;
+  } else {
+    arabico += valor;
+  }
+  valorAnterior = valor;
+    }
+  return arabico;
 }
  
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -6,14 +6,15 @@
 TEST_CASE( "Conversão de números romanos para arábicos") {
     SECTION("Teste com algarismos simples") {
         REQUIRE(converterRomanoParaArabico("I") == 1 );
-    
         REQUIRE(converterRomanoParaArabico("V") == 5 );
+    }
 
+    SECTION("Teste com algarismos compostos") {
+        REQUIRE(converterRomanoParaArabico("IV") == 4);
+        REQUIRE(converterRomanoParaArabico("IX") == 9);
 
     }
-    
-
-    
+     
 }
 
 
