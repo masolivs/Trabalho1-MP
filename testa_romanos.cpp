0b956c9f6c2751d06353aa980a385c07498a3470
masolivs <marianasoli621@gmail.com>
2024-04-25 21:48:45 -0300

    Últimas correções + uso do make clean

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -1,11 +1,11 @@
 // Copyright 2024 <Mariana Oliveira>
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "./catch.hpp"
 #include "./romanos.hpp"
 
 
-TEST_CASE( "Conversão de números romanos para arábicos") {
+TEST_CASE("Conversão de números romanos para arábicos") {
     SECTION("Teste com algarismos simples") {
         REQUIRE(converterRomanoParaArabico("I") == 1);
         REQUIRE(converterRomanoParaArabico("V") == 5);
@@ -33,7 +33,7 @@ TEST_CASE( "Conversão de números romanos para arábicos") {
         REQUIRE(converterRomanoParaArabico("XD") == -1);
         REQUIRE(converterRomanoParaArabico("IM") == -1);
         REQUIRE(converterRomanoParaArabico("IIII") == -1);
-    } 
+    }
 }
 
 
